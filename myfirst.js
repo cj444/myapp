7ec95214cb1302448d5fb5fb904ec58d5954e3f1
cj444 <42742847+cj444@users.noreply.github.com>
2018-08-28 00:25:46 +0530

    Add files via upload

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,6 @@
 var http = require('http');
-
+const PORT = process.env.PORT || 5000
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World out side!');
-}).listen(3080); 
+}).listen(PORT); 
